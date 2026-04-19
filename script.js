e625da06e18bbb8a48cc3bd0542d301171bad868
mosesliusisheng-a11y <mosesliusisheng@gmail.com>
2026-04-19 09:08:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let positionX = 350;
 let speed = 4;
 
 // Floors
-const platformY = 81.5;
-const groundY = 1.5;
+const platformY = 80.5;
+const groundY = 0.5;
 
 // State
 let currentFloor = "platform"; // "platform" or "ground"
